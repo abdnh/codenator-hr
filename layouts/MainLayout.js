e7d5fc8fa866a7c87ee470d377cc6a5b01e2f075
Abdo <abd.nh25@gmail.com>
2022-07-29 11:18:37 +0300

    Add MainLayout

---
+++ layouts/MainLayout.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import styles from '../styles/MainLayout.module.css';
+import Navbar from "../components/Navbar";
+import { SITE_NAME } from "../lib/common";
+
+
+export default function MainLayout({ subtitle, children }) {
+    let title = `${SITE_NAME} HR`;
+    if (subtitle) {
+        title += ` - ${subtitle}`;
+    }
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={title} />
+                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+            </Head>
+            <Navbar />
+            <main className={styles.main}>
+                {children}
+            </main>
+        </div>
+    )
+}
