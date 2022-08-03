d4891d5b7a5ea035a0ff50770089724b8438118c
Abdo <abd.nh25@gmail.com>
2022-08-03 10:01:55 +0300

    Start work on more homepage sections

--- components/Button.js
+++ components/Button.js
@@ -1,5 +1,5 @@
 import styles from "../styles/Button.module.scss";
 
-export default function Button({ children }) {
-    return <button className={styles.button}>{children}</button>
+export default function Button({ children, buttonStyle }) {
+    return <button className={`${styles.button} ${styles[buttonStyle]}`}>{children}</button>
 }
