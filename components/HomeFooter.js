9cc5ff5ac29ec51524da56d58da84eb5954fe2fb
Abdo <abd.nh25@gmail.com>
2022-08-03 23:04:47 +0300

    Fix footer's grid not working properly due to missing row

--- components/HomeFooter.js
+++ components/HomeFooter.js
@@ -1,10 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 import styles from "../styles/HomeFooter.module.scss"
 
 
 function FooterColumn({ header, items }) {
-    return <Col className={styles.col}>
+    return <Col className={styles.col} xs={6} lg={3}>
         <p className={styles.colheader}>{header}</p>
         <ul className={styles.itemlist}>
             {
@@ -21,11 +23,11 @@ export default function HomeFooter() {
             Got you interested?<br />
             Start by completing your <a href="/profile">profile</a>.
         </p>
-        <Container fluid className={styles.linkscontainer}>
-            <Col className={`${styles.banner} ${styles.col}`}><a href="#">Codenator</a></Col>
+        <Row className={styles.linkscontainer}>
+            <Col xs={12} lg={6} className={`${styles.banner} ${styles.col} mb-5`}><a href="#">Codenator</a></Col>
             <FooterColumn header="Support" items={['Docs', 'Contact us']} />
             <FooterColumn header="Company" items={['About', 'Blog']} />
-        </Container>
+        </Row>
         <Container fluid className={styles.copyrightBar}></Container>
     </div>
 }
