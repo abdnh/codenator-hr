e765660c12bb78879b22183a35e80a4a8f73b935
Abdo <abd.nh25@gmail.com>
2022-08-16 12:25:22 +0300

    Fix PlusButton not accepting onClick handler

--- components/ant/PlusButton.js
+++ components/ant/PlusButton.js
@@ -1,10 +1,10 @@
 import { Button } from "antd";
 import { PlusOutlined } from '@ant-design/icons';
 
-export default function PlusButton() {
+export default function PlusButton(props) {
     return (
         <>
-            <Button icon={<PlusOutlined />} type="dashed" block={true} />
+            <Button icon={<PlusOutlined />} type="dashed" block={true} {...props} />
             <style global jsx>{`
                 .ant-btn {
                     border-color: #696969 !important;
