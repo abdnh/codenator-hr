0da33e40fea1f5843e08af08587a669ba80d17dd
Abdo <abd.nh25@gmail.com>
2022-09-03 16:53:42 +0300

    Remove useless link

--- pages/index.js
+++ pages/index.js
@@ -619,9 +619,9 @@ function Footer() {
       <div className="row gx-5">
         <div className="col-lg-4 col-md-6 footer-about">
           <div className="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary p-4">
-            <a href="/" className="navbar-brand">
+            <div className="navbar-brand">
               <h1 className="m-0 text-white"><i className="me-2"><FontAwesomeIcon icon={faUserTie} /></i>Codenator</h1>
-            </a>
+            </div>
             <p className="mt-3 mb-4">Lorem diam sit erat dolor elitr et, diam lorem justo amet clita stet eos sit. Elitr dolor duo lorem, elitr clita ipsum sea. Diam amet erat lorem stet eos. Diam amet et kasd eos duo.</p>
             <form action="">
               <div className="input-group">
@@ -696,7 +696,6 @@ function Footer() {
 
 export async function getStaticProps() {
   const jobs = await getRandomJobs(5);
-  console.log('jobs', jobs)
   return {
     props: {
       jobs,
